751555d7539f1ac5b69a6f2f16ecfdecc6ed98fb
Sumalata <sumasbshetty@gmail.com>
2026-06-09 11:04:25 +0530

    Add files via upload

---
+++ SQL cls.sql
@@ -0,0 +1,16 @@
+-- This is single line comment
+/* This is
+multiple
+line
+comment */
+-- DDL
+create database emp
+create database emp1;
+create database emp2;
+-- drop
+drop database emp1;
+drop database emp2;
+drop database emp;
+
+show databases;
+show databases;
